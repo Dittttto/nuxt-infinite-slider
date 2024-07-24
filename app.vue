4e30d4f53c06deff2ff8560d2bbbfbd1b66d629f
DevFrog92 <yongsu1481@gmail.com>
2024-07-24 22:30:01 +0900

    feat: implement infinite slider

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <NuxtPage />
 </template>
+
+<style>
+html,
+body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0;
+    background-color: #e5e5e5;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="container">
+        <div
+            class="slider"
+            style="--width: 150px; --height: 150px; --quantity: 5"
+        >
+            <div class="slider-list">
+                <div
+                    v-for="i in 5"
+                    :key="i"
+                    :style="`--position: ${i}`"
+                    class="item"
+                >
+                    <img
+                        :src="`/assets/image/sample1.jpeg`"
+                        alt="slider image"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.container {
+    padding: 10rem;
+    overflow: hidden;
+    width: 100%;
+    margin: auto;
+}
+
+.slider {
+    width: 700px;
+    height: var(--height);
+    overflow: hidden;
+}
+
+.slider .slider-list {
+    display: flex;
+    width: 100%;
+    position: relative;
+}
+
+.slider .slider-list .item {
+    width: var(--width);
+    height: var(--height);
+    position: absolute;
+    left: 100%;
+    animation: autoRun 15s linear infinite;
+    transition: filter 0.5s;
+    animation-delay: calc((15s / var(--quantity)) * (var(--position) - 1)) !important;
+}
+
+.slider .slider-list .item img {
+    width: 100%;
+}
+
+@keyframes autoRun {
+    from {
+        left: 100%;
+    }
+    to {
+        left: calc(var(--width) * -1);
+    }
+}
+
+.slider:hover .item {
+    animation-play-state: paused !important;
+    filter: grayscale(1);
+}
+
+.slider .item:hover {
+    filter: grayscale(0);
+}
+</style>
